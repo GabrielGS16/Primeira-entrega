476dc8bf850428d0faaa6fd3ac8ef58269224b50
GabrielGS16 <gabrielgirao760@gmail.com>
2025-05-27 18:02:28 -0300

    Add files via upload

---
+++ login/conexao.php
@@ -0,0 +1,11 @@
+<?php
+$usuario = 'root';
+$senha = '';
+$database = 'login';
+$host ='localhost';
+
+$mysqli = new mysqli($host, $usuario, $senha, $database);
+if($mysqli->error) {
+    die("Falaha ao conectar no banco de dados: " . $mysqli->error);
+}
+?>
